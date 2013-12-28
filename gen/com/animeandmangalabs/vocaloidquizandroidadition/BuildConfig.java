56c6082271ba2c3af9b118fb966786ec4ecd937e
Jeroen Mathon <deathslycer@gmail.com>
2013-12-28 14:16:40 +0100

    Vocaloid Quiz init

---
+++ gen/com/animeandmangalabs/vocaloidquizandroidadition/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.animeandmangalabs.vocaloidquizandroidadition;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
