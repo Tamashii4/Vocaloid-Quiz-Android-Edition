2a1dd6fa8261e20fe04b7b4987e921f66415e795
Jeroen Mathon <deathslycer@gmail.com>
2013-12-28 21:18:59 +0100

    Intergrated quiz_engine with Quiz_01 class
    Still in need for some structure.

--- src/com/animeandmangalabs/vocaloidquizandroidedition/Quiz_01.java
+++ src/com/animeandmangalabs/vocaloidquizandroidedition/Quiz_01.java
@@ -1,15 +1,16 @@
 package com.animeandmangalabs.vocaloidquizandroidedition;
 
-import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
 public class Quiz_01 extends Activity {
 	
 		//Quiz init[start]
@@ -19,16 +20,28 @@ public class Quiz_01 extends Activity {
 		static TextView quiz_question_text;	//TextView's of the interface
 		static TextView tCorrect;
 		static TextView tIncorrect;
-		
+		static int iQnum = 0;
+		static int iCorrect = 0;
+		static int iIncorrect = 0;
 		//Quiz init[end]
-	@Override
+	
+
+		public void reset_values(){
+			iQnum = 0;
+			iCorrect = 0;
+			iIncorrect = 0;
+			
+		}
+				
+				
+			
+		
+		@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz_01);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		
-		//Link view items[start]
 		btn_a = (Button) findViewById(R.id.quiz_button_a);
 		btn_b = (Button) findViewById(R.id.quiz_button_b);
 		quiz_img_view = (ImageView) findViewById(R.id.quiz_image);
@@ -36,17 +49,19 @@ public class Quiz_01 extends Activity {
 		tCorrect = (TextView) findViewById(R.id.tCorrect);
 		tIncorrect = (TextView) findViewById(R.id.tIncorrect);
 		//Link view items[end]
-		quiz_engine.main();
+		reset_values();
+		quiz_questions();
 		
 	}
-
+			
+		
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
 	 */
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	private void setupActionBar() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
+			
 		}
 	}
 
@@ -68,10 +83,59 @@ public class Quiz_01 extends Activity {
 			//
 			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
 			//
+			
 			NavUtils.navigateUpFromSameTask(this);
+			
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
-}
+
+
+
+
+
+	public static void Correct(){//When user's correct
+	iCorrect = iCorrect +1;
+	tCorrect.setText("Correct: " + iCorrect);
+	}
+	public static void Incorrect(){//When user's incorrect
+	iIncorrect = iIncorrect + 1;
+	tIncorrect.setText("Incorrect: " + iIncorrect);
+	}
+
+
+	public static void quiz_questions(){
+
+		if(iQnum == 0){//Quiz Question num(1)
+		quiz_question_text.setText(R.string.q1_1);
+		quiz_img_view.setImageResource(R.drawable.miku_2);
+		btn_a.setText("Hatsune Miku");
+		btn_b.setText("Kagame Rin");
+		
+			btn_a.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Correct();
+				}
+			});
+			btn_b.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Incorrect();
+				}
+			});
+		}
+	}
+
+	}
+	
+	
+	
+
+	
